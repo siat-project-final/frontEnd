b70a0ed8584f7304ca46226420afbd3d2a445ea7
shin-hyoung <pshconet@gmail.com>
2025-06-08 21:04:48 +0900

    connection complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CalendarPage from "./CalendarPage";
+
+function App() {
+  return (
+    <div className="App">
+      <CalendarPage />
+    </div>
+  );
+}
+
+export default App;
