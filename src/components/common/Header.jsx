c24b149a3b8296eb1ca238c40d3a93574299aaaf
박신형 <pshconet99@gmail.com>
2025-06-30 15:35:45 +0900

    style:dropdown 기본 완성

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -30,54 +30,79 @@ const Header = ({ menuType }) => (
       </NavLink>
 
       <nav id="navmenu" className="navmenu">
-        <ul>
+        <ul style={{ gap: window.innerWidth >= 1200 ? '3rem' : undefined }}>
           <li className="dropdown">
             <NavLink to="/home" className={({ isActive }) => (isActive ? 'active' : '')}>
               Home
             </NavLink>
           </li>
           <li className="dropdown">
-            <NavLink to="/challenge" className={({ isActive }) => (isActive ? 'active' : '')}>
+            <NavLink to="/challenge" className={({ isActive }) => (isActive ? 'active' : '')} style={({ isActive }) => ({ fontSize: isActive ? '1.2rem' : '1.2rem', fontWeight: isActive ? 700 : 500 })}>
               Challenge
             </NavLink>
-            <ul className="dropdown-menu" style={{ width: '110px', minWidth: '110px', maxWidth: '120px' }}>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/challenge/daily" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>일일 챌린지</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/challenge/ranking" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>챌린지 랭킹</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/challenge/review" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>종합 챌린지</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/mypage/challenge-history" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>챌린지 히스토리</NavLink></li>
+            <ul className="dropdown-menu" style={{ marginTop: '28px',
+                  left: '-5px' ,
+                  top: '50%',
+                  width: '60px',
+                  minWidth: '120px',
+                  maxWidth: '120px',
+                  padding: 0}}>
+              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/challenge/daily" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>일일 챌린지</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/challenge/ranking" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>챌린지 랭킹</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/challenge/review" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>종합 챌린지</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/challenge-history" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>챌린지 히스토리</NavLink></li>
             </ul>
           </li>
           <li className="dropdown">
             <NavLink
               to="/mentoring/mentors"
               className={({ isActive }) => (isActive || menuType === 'mentoring' ? 'active' : '')}
+              style={({ isActive }) => ({ fontSize: isActive ? '1.2rem' : '1.2rem', fontWeight: isActive ? 700 : 500 })}
             >
               Mentoring
             </NavLink>
-            <ul className="dropdown-menu" style={{ width: '110px', minWidth: '110px', maxWidth: '120px' }}>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/mentoring/mentors" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>멘토 목록</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/mentoring/register" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>예약 내역</NavLink></li>
+            <ul className="dropdown-menu" style={{ marginTop: '28px',
+                  left: '-5px' ,
+                  top: '50%',
+                  width: '60px',
+                  minWidth: '120px',
+                  maxWidth: '120px',
+                  padding: 0 }}>
+              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/mentoring/mentors" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>멘토 목록</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mentoring/register" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>예약 내역</NavLink></li>
             </ul>
           </li>
           <li className="dropdown">
-            <NavLink to="/study" className={({ isActive }) => (isActive ? 'active' : '')}>
+            <NavLink to="/study" className={({ isActive }) => (isActive ? 'active' : '')} style={({ isActive }) => ({ fontSize: isActive ? '1.2rem' : '1.2rem', fontWeight: isActive ? 700 : 500 })}>
               Studylog
             </NavLink>
-            <ul className="dropdown-menu" style={{ width: '110px', minWidth: '110px', maxWidth: '110px' }}>
-              <li style={{ width: '110px', minWidth: '110px' }}><NavLink to="/study" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>학습 일지 목록</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/study/write" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>학습 일지 작성</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/study/public" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>공유 학습 일지</NavLink></li>
+            <ul className="dropdown-menu" style={{ marginTop: '28px',
+                  left: '-9px' ,
+                  top: '50%',
+                  width: '60px',
+                  minWidth: '120px',
+                  maxWidth: '120px',
+                  padding: 0 }}>
+              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/study" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>학습 일지 목록</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/study/write" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>학습 일지 작성</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/study/public" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>공유 학습 일지</NavLink></li>
             </ul>
           </li>
           <li className="dropdown">
-            <NavLink to="/mypage" className={({ isActive }) => (isActive ? 'active' : '')}>
+            <NavLink to="/mypage" className={({ isActive }) => (isActive ? 'active' : '')} style={({ isActive }) => ({ fontSize: isActive ? '1.2rem' : '1.2rem', fontWeight: isActive ? 700 : 500 })}>
               MyPage
             </NavLink>
-            <ul className="dropdown-menu" style={{ width: '110px', minWidth: '110px', maxWidth: '110px' }}>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/mypage" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>마이 프로필</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/mypage/mentoring-history" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>멘토링 히스토리</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/mypage/statistics" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>통계</NavLink></li>
-              <li style={{ width: '110px', minWidth: '120px' }}><NavLink to="/mypage/challenge-history" className="dropdown-item" style={{ fontSize: '12px', padding: '8px 12px', textDecoration: 'none' }}>챌린지 히스토리</NavLink></li>
+            <ul className="dropdown-menu" style={{ marginTop: '28px',
+                  left: '-15px' ,
+                  top: '50%',
+                  width: '60px',
+                  minWidth: '120px',
+                  maxWidth: '120px',
+                  padding: 0 }}>
+              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/mypage" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>마이 프로필</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/mentoring-history" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>멘토링 히스토리</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/statistics" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>통계</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/challenge-history" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>챌린지 히스토리</NavLink></li>
             </ul>
           </li>
           <li className="alarm-item" style={{ 
