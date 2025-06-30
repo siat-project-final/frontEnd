30a6a57b2f55b8070e36a102cfe9cdfee2d853fb
박신형 <pshconet99@gmail.com>
2025-06-30 17:03:16 +0900

    style: dropdown 디자인완료

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = ({ menuType }) => (
+  <>
+  {/* .dropdown-item.active 배경색을 초록 계열 #dff7e6로 덮어쓰기 */}
+  <style>
+    {`
+      .dropdown-item.active,
+      .dropdown-item:active {
+        background-color: #f0fdf4 !important;
+      }
+    `}
+  </style>
+  
   <header id="header" className="header d-flex align-items-center sticky-top">
     <div className="container-fluid container-xl position-relative d-flex align-items-center">
             {/* 투명한 더미 요소 - 로고 크기만큼 공간 차지 */}
@@ -30,7 +41,7 @@ const Header = ({ menuType }) => (
       </NavLink>
 
       <nav id="navmenu" className="navmenu">
-        <ul style={{ gap: window.innerWidth >= 1200 ? '3rem' : undefined }}>
+        <ul style={{ gap: window.innerWidth >= 1200 ? '3rem' : undefined }} >
           <li className="dropdown">
             <NavLink to="/home" className={({ isActive }) => (isActive ? 'active' : '')}>
               Home
@@ -41,16 +52,16 @@ const Header = ({ menuType }) => (
               Challenge
             </NavLink>
             <ul className="dropdown-menu" style={{ marginTop: '28px',
-                  left: '-5px' ,
+                  left: '-30px' ,
                   top: '50%',
                   width: '60px',
-                  minWidth: '120px',
-                  maxWidth: '120px',
+                  minWidth: '170px',
+                  maxWidth: '170px',
                   padding: 0}}>
-              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/challenge/daily" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>일일 챌린지</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/challenge/ranking" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>챌린지 랭킹</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/challenge/review" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>종합 챌린지</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/challenge-history" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>챌린지 히스토리</NavLink></li>
+              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/challenge/daily" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none'}}>일일 챌린지</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/challenge/ranking" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none'}}>챌린지 랭킹</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/challenge/review" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none'}}>종합 챌린지</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/challenge-history" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>챌린지 히스토리</NavLink></li>
             </ul>
           </li>
           <li className="dropdown">
@@ -62,14 +73,14 @@ const Header = ({ menuType }) => (
               Mentoring
             </NavLink>
             <ul className="dropdown-menu" style={{ marginTop: '28px',
-                  left: '-5px' ,
+                  left: '-28px' ,
                   top: '50%',
                   width: '60px',
-                  minWidth: '120px',
-                  maxWidth: '120px',
+                  minWidth: '170px',
+                  maxWidth: '170px',
                   padding: 0 }}>
-              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/mentoring/mentors" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>멘토 목록</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/mentoring/register" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>예약 내역</NavLink></li>
+              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/mentoring/mentors" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>멘토 목록</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mentoring/register" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>예약 내역</NavLink></li>
             </ul>
           </li>
           <li className="dropdown">
@@ -77,15 +88,15 @@ const Header = ({ menuType }) => (
               Studylog
             </NavLink>
             <ul className="dropdown-menu" style={{ marginTop: '28px',
-                  left: '-9px' ,
+                  left: '-35px' ,
                   top: '50%',
                   width: '60px',
-                  minWidth: '120px',
-                  maxWidth: '120px',
+                  minWidth: '170px',
+                  maxWidth: '170px',
                   padding: 0 }}>
-              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/study" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>학습 일지 목록</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/study/write" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>학습 일지 작성</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/study/public" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>공유 학습 일지</NavLink></li>
+              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/study" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>학습 일지 목록</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/study/write" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>학습 일지 작성</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/study/public" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>공유 학습 일지</NavLink></li>
             </ul>
           </li>
           <li className="dropdown">
@@ -93,16 +104,16 @@ const Header = ({ menuType }) => (
               MyPage
             </NavLink>
             <ul className="dropdown-menu" style={{ marginTop: '28px',
-                  left: '-15px' ,
+                  left: '-38px' ,
                   top: '50%',
                   width: '60px',
-                  minWidth: '120px',
-                  maxWidth: '120px',
+                  minWidth: '170px',
+                  maxWidth: '170px',
                   padding: 0 }}>
-              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/mypage" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>마이 프로필</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/mentoring-history" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>멘토링 히스토리</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/statistics" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>통계</NavLink></li>
-              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/challenge-history" className="dropdown-item" style={{ fontSize: '14px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '120px', display: 'block', borderBottom: 'none' }}>챌린지 히스토리</NavLink></li>
+              <li style={{ minWidth: '40px', marginTop: '8px' }}><NavLink to="/mypage" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>마이 프로필</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/mentoring-history" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>멘토링 히스토리</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/statistics" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>통계</NavLink></li>
+              <li style={{ minWidth: '40px' }}><NavLink to="/mypage/challenge-history" className="dropdown-item" style={{ fontSize: '18px', padding: '8px 12px', textDecoration: 'none', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '170px', display: 'block', borderBottom: 'none' }}>챌린지 히스토리</NavLink></li>
             </ul>
           </li>
           <li className="alarm-item" style={{ 
@@ -142,6 +153,7 @@ const Header = ({ menuType }) => (
       </NavLink>
     </div>
   </header>
+  </>
 );
 
 export default Header;
