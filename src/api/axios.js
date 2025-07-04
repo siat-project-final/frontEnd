39931e01ee54a6e86f526cc5e86842b5c77f08ad
kth <kth1017@github.com>
2025-07-04 11:14:04 +0900

    [fix] 배포 nginx 테스트

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://13.125.170.63:8087/v1',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://siathub.com/v1',
   withCredentials: true,
 });
 
